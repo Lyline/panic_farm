e97b8b812ad79870fcf1b1080f1607a1740e63a7
Quesne Greg <greg.quesne@gmail.com>
2025-09-11 12:33:21 +0200

    68c296a98298cb38408f29d4 Dev - Définition de la zone de déplacement du joueur

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,26 +5,29 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func get_input():
+	position.x=clamp(position.x,10,505)
+	position.y=clamp(position.y,15,480)
+	
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity= direction*SPEED
-	
+
 	if direction.x > 0 :
 		$PlayerAnimation.flip_h=false
 		_animation.play("player_walk_h")
-		
-	elif direction.x < 0 : 
+
+	elif direction.x < 0 :
 		$PlayerAnimation.flip_h=true
-		_animation.play("player_walk_h")	
-	
+		_animation.play("player_walk_h")
+
 	elif direction.y < 0:
 		_animation.play("player_walk_up")
-	
+
 	elif direction.y > 0:
 		_animation.play("player_walk_down")
 
 	else :
-		_animation.play("player_idle")	
-	
+		_animation.play("player_idle")
+
 
 func _physics_process(delta: float) -> void:
 	get_input()
